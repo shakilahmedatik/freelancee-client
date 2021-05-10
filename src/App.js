585dc450311c510b2f4809fe2b83810aa71bd72a
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-05-11 04:39:21 +0600

    Register Form Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
-import Home from './components/Home/Home'
+import Home from './components/Service/Home'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import TopNav from './components/Shared/NavBar/TopNav'
 
---
+++ src/components/Shared/Form/RegisterForm.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+const RegisterForm = ({
+  handleSubmit,
+  name,
+  email,
+  password,
+  setName,
+  setEmail,
+  setPassword,
+}) => {
+  return (
+    <form onSubmit={handleSubmit} className='mt-3'>
+      <div className='form-group mb-3'>
+        <label className='form-label'>Your name</label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter name'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </div>
+
+      <div className='form-group mb-3'>
+        <label className='form-label'>Email address</label>
+        <input
+          type='email'
+          className='form-control'
+          placeholder='Enter email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className='form-group mb-3'>
+        <label className='form-label'>Password</label>
+        <input
+          type='password'
+          className='form-control'
+          placeholder='Enter password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button className='btn btn-primary'>Submit</button>
+    </form>
+  )
+}
+
+export default RegisterForm
