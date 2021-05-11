7d1fde6cb527752411fcc275c543de18394cd63b
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-05-11 15:04:28 +0600

    dotenv added

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
   const handleSubmit = async e => {
     e.preventDefault()
     try {
-      const res = await axios.post(`http://localhost:8000/api/register`, {
+      const res = await axios.post(`${process.env.REACT_APP_API}/register`, {
         name,
         email,
         password,
